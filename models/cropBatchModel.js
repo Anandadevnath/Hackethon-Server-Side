4c84bd512b4e07b05181f0019b5add2ce63cfea7
ARNOB663 <arnob229x@gmail.com>
2025-11-29 10:17:45 +0600

    changes on cropbatchmodel add more crop option to select from

--- models/cropBatchModel.js
+++ models/cropBatchModel.js
@@ -2,7 +2,79 @@ import mongoose from "mongoose";
 
 const cropBatchSchema = new mongoose.Schema({
   farmerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Farmer', required: true },
-  cropType: { type: String, enum: ['Paddy', 'Rice'], required: true },
+  cropType: { 
+    type: String, 
+    enum: [
+      // Grains & Cereals (শস্য)
+      'Paddy',        // ধান
+      'Rice',         // চাল
+      'Wheat',        // গম
+      'Maize',        // ভুট্টা
+      'Barley',       // যব
+      
+      // Cash Crops (অর্থকরী ফসল)
+      'Jute',         // পাট
+      'Sugarcane',    // আখ
+      'Cotton',       // তুলা
+      'Tobacco',      // তামাক
+      'Tea',          // চা
+      
+      // Vegetables (সবজি)
+      'Potato',       // আলু
+      'Onion',        // পেঁয়াজ
+      'Garlic',       // রসুন
+      'Tomato',       // টমেটো
+      'Chili',        // মরিচ
+      'Brinjal',      // বেগুন
+      'Cabbage',      // বাঁধাকপি
+      'Cauliflower',  // ফুলকপি
+      'Carrot',       // গাজর
+      'Radish',       // মুলা
+      'Spinach',      // পালং শাক
+      'Pumpkin',      // কুমড়া
+      'Bottle Gourd', // লাউ
+      'Bitter Gourd', // করলা
+      'Cucumber',     // শসা
+      'Okra',         // ঢেঁড়স
+      'Beans',        // শিম
+      'Ginger',       // আদা
+      'Turmeric',     // হলুদ
+      'Coriander',    // ধনিয়া
+      
+      // Pulses (ডাল)
+      'Lentils',      // মসুর ডাল
+      'Chickpea',     // ছোলা
+      'Mung Bean',    // মুগ ডাল
+      'Black Gram',   // মাষকলাই
+      'Peas',         // মটরশুঁটি
+      
+      // Oilseeds (তৈলবীজ)
+      'Mustard',      // সরিষা
+      'Groundnut',    // চীনাবাদাম
+      'Sesame',       // তিল
+      'Sunflower',    // সূর্যমুখী
+      
+      // Fruits (ফল)
+      'Mango',        // আম
+      'Banana',       // কলা
+      'Jackfruit',    // কাঁঠাল
+      'Litchi',       // লিচু
+      'Papaya',       // পেঁপে
+      'Guava',        // পেয়ারা
+      'Watermelon',   // তরমুজ
+      'Pineapple',    // আনারস
+      'Coconut',      // নারিকেল
+      'Orange',       // কমলা
+      'Lemon',        // লেবু
+      
+      // Others (অন্যান্য)
+      'Betel Leaf',   // পান
+      'Betel Nut',    // সুপারি
+      'Vegetables',   // শাকসবজি (mixed)
+      'Other'         // অন্যান্য
+    ], 
+    required: true 
+  },
   estimatedWeightKg: { type: Number, required: true, min: 0 },
   harvestDate: { type: Date, required: true },
   storageLocation: {
--- validators/cropBatchValidate.js
+++ validators/cropBatchValidate.js
@@ -1,7 +1,28 @@
 import * as yup from 'yup';
 
+// Common crops grown in Bangladesh (60+ types)
+const CROP_TYPES = [
+  // Grains & Cereals
+  'Paddy', 'Rice', 'Wheat', 'Maize', 'Barley',
+  // Cash Crops
+  'Jute', 'Sugarcane', 'Cotton', 'Tobacco', 'Tea',
+  // Vegetables
+  'Potato', 'Onion', 'Garlic', 'Tomato', 'Chili', 'Brinjal', 'Cabbage',
+  'Cauliflower', 'Carrot', 'Radish', 'Spinach', 'Pumpkin', 'Bottle Gourd',
+  'Bitter Gourd', 'Cucumber', 'Okra', 'Beans', 'Ginger', 'Turmeric', 'Coriander',
+  // Pulses
+  'Lentils', 'Chickpea', 'Mung Bean', 'Black Gram', 'Peas',
+  // Oilseeds
+  'Mustard', 'Groundnut', 'Sesame', 'Sunflower',
+  // Fruits
+  'Mango', 'Banana', 'Jackfruit', 'Litchi', 'Papaya', 'Guava',
+  'Watermelon', 'Pineapple', 'Coconut', 'Orange', 'Lemon',
+  // Others
+  'Betel Leaf', 'Betel Nut', 'Vegetables', 'Other'
+];
+
 export const cropBatchSchema = yup.object().shape({
-  cropType: yup.string().oneOf(['Paddy','Rice']).required('cropType is required'),
+  cropType: yup.string().oneOf(CROP_TYPES).required('cropType is required'),
   estimatedWeightKg: yup.number().min(0).required('estimatedWeightKg is required'),
   harvestDate: yup.date().required('harvestDate is required'),
   storageLocation: yup.object().shape({
@@ -24,7 +45,7 @@ export const validateCropBatch = (schema) => (req, res, next) => {
 
 // Partial update schema for editing an existing batch
 export const cropBatchUpdateSchema = yup.object().shape({
-  cropType: yup.string().oneOf(['Paddy','Rice']).optional(),
+  cropType: yup.string().oneOf(CROP_TYPES).optional(),
   estimatedWeightKg: yup.number().min(0).optional(),
   harvestDate: yup.date().optional(),
   storageLocation: yup.object().shape({
